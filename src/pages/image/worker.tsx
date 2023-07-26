52089235a6dfda1575a4a9e76a9845544f3fcecb
ukyi <ukyi.js@gmail.com>
2023-07-26 17:56:45 +0900

    ✨ Worker 컴포넌트 추가

--- src/pages/image/worker.tsx
+++ src/pages/image/worker.tsx
@@ -1,7 +1,47 @@
+import { useState } from 'react';
+import ImageContainer from '@/components/imageContainer';
+import { useImageLoadWorker } from '@/hooks/useImageLoadWorker';
+import { useRandomImage } from '@/hooks/useRandomImage';
+
+enum UpdateType {
+  ALL = 'ALL',
+  INCREMENTALLY = 'INCREMENTALLY',
+}
+
 export default function Worker(): JSX.Element {
+  const [imageLength, setImageLength] = useState(60);
+  const [updateType, setUpdateType] = useState(UpdateType.ALL);
+  const images = useRandomImage(imageLength);
+  const { imageBlobs, maxWorkers } = useImageLoadWorker({ images, incrementalUpdate: updateType === UpdateType.INCREMENTALLY });
+
+  const onUpdateTypeChange = async (type: UpdateType) => {
+    const length = imageLength;
+
+    setUpdateType(type);
+    setImageLength(0);
+
+    await new Promise(resolve => setTimeout(resolve, 100));
+    setImageLength(length);
+  };
+
   return (
-    <section>
-      worker
+    <section className="flex flex-col gap-4 w-full h-full">
+      <form className="flex justify-center gap-4">
+        {Object.values(UpdateType).map((type, i) => (
+          <label key={i}>
+            <input type="radio" name="update-type" value={type} checked={updateType === type} onChange={() => onUpdateTypeChange(type)} />
+            {type.toLowerCase()}
+          </label>
+        ))}
+      </form>
+      <ImageContainer>
+        {imageBlobs.map((image, i) => (
+          <span key={i} className={`inline-flex justify-center items-center rounded w-24 h-36 bg-cover transition-all ease-linear delay-1000 ${image === undefined && 'bg-gray-300'} ${image === null && 'accent-red-600'}`} style={{ backgroundImage: image && `url(${image})` }}>
+            {image === null && 'error'}
+            {image === undefined && 'loading...'}
+          </span>
+        ))}
+      </ImageContainer>
     </section>
   );
 }
