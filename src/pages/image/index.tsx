83dd1a55bb0fa1147ef15261ab0c2771afa15324
ukyi <ukyi.js@gmail.com>
2023-07-26 16:57:53 +0900

    ✨ useRandomImage 추가

--- src/pages/image/index.tsx
+++ src/pages/image/index.tsx
@@ -1,7 +1,14 @@
+import ImageContainer from '@/components/imageContainer';
+import { useRandomImage } from '@/hooks/useRandomImage';
+
 export default function Image(): JSX.Element {
+  const images = useRandomImage(100);
+
   return (
-    <section>
-      Image
+    <section className="w-full h-full">
+      <ImageContainer>
+        {images.map((image, i) => <img key={i} src={image} alt={`${i}-image`} className="w-24 rounded" />)}
+      </ImageContainer>
     </section>
   );
 }
